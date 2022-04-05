ff087f9e7f80aa3f841b6327274dd7ff4b20e145
ileanachamorro <ic.chamorro07@gmail.com>
2022-04-05 20:52:33 -0300

    Variables

---
+++ 1.Tipos de Datos/variables.py
@@ -0,0 +1,18 @@
+#Variables
+#Reglas para nombrar variables
+#Se considera Buena practica usar nombre en minuscula
+#Evita usar palabras con significado especial como "list" o "str"
+
+#Python usa nomenclatura Dinamica
+#Significa que puedes re-asignar variables a otros tipos de datos
+#Esto hace python bastante flexible al momento de asignar variables
+
+Mis_casas = 2
+Mis_casas = ["casa1","casa2"] #Esto es correcto
+
+
+vida = 100
+
+danio = 15
+
+print(vida - danio)
