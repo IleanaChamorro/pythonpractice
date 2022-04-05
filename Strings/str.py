84c7f36038a1cd8bc2f0c3c189a9f96e185714d3
ileanachamorro <ic.chamorro07@gmail.com>
2022-04-05 20:06:59 -0300

    Manipulacion string

---
+++ Strings/str.py
@@ -0,0 +1,15 @@
+#Manipulación cadenas de caracteres (strings)
+
+mensaje = "Hola"
+
+mensaje += " "
+
+mensaje += "Amigo"
+
+print(mensaje) 
+
+mensaje_dos = "Hola Amigo"
+
+buscar_subcadena = mensaje_dos.find("Amigo")
+
+print(buscar_subcadena)
