3c7add7765952dfdc5a03d03d04c12fb77af8313
ileanachamorro <ic.chamorro07@gmail.com>
2022-04-16 13:34:40 -0300

    Ejercicio practico #2

---
+++ Ejercicios/ejercicio-2.py
@@ -0,0 +1,17 @@
+#Desarrollar un programa que solicite un número entero desde teclado al usuario, posteriormente, el programa deberá determinar e indicar a través de un mensaje, si el número introducido es par o impar
+
+#Requerimientos indispensables:
+#El mensaje en pantalla deberá mostrar la frase el número es par o impar, junto con el número que el usuario introdujo desde teclado. por ejemplo:
+#"El número 8 es par"
+#"El número 5 es impar"
+
+print("*************************")
+print("* Programa que determina si un número es par o impar")
+print("*************************")
+
+numero = int(input("Por favor introduce un número entero: "))
+
+if numero % 2 == 0:
+    print("El número ", numero, " es par.")
+elif numero % 2 == 1:
+    print("El número ", numero, " es impar")
