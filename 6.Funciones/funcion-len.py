821a7ee6d94cdaaa13446262a11d4fd74bfff5b2
ileanachamorro <ic.chamorro07@gmail.com>
2022-04-26 21:00:35 -0300

    La función len()

---
+++ 6.Funciones/funcion-len.py
@@ -0,0 +1,13 @@
+#En python, la función len() nos permite obtener la longitud de una cadena de caracteres, o bien, el número de elementos que componen un objeto.
+#La sintaxis correcta para poder utilizar la función len(), es la siguiente:
+#len("Hola")
+
+#Comportamiento de la función len()
+print(len("Hola"))
+
+#Opción 1
+print("Hola tiene", len("Hola"), "caracteres")
+
+#Opcion 2
+longitud = len("Ejemplo cadena")
+print("Ejemplo cadena tiene", longitud, "caracteres")
