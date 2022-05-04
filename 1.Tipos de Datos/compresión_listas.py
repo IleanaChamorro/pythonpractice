b868fc3c6863370b8953697328abb403314a2907
ileanachamorro <ic.chamorro07@gmail.com>
2022-05-04 18:55:22 -0300

    Listas de comprension

---
+++ 1.Tipos de Datos/compresión_listas.py
@@ -0,0 +1,21 @@
+#Manera unica de crear una lista de python rapidamente:
+
+#Si te encuentras usando un ciclo for con .append() para crear una lista, puedes usar una comprension en su lugar
+
+#mi_cadena = 'hola'
+
+#mi_lista = []
+
+#Para cada letra en la cadena imprimirla como lista
+#for letter in mi_cadena:
+#    mi_lista.append(letter)
+#    print(mi_lista)
+
+#Lista del 0 al 11
+#mi_lista = [x for x in range(0,11)]
+#print(mi_lista)
+
+#celcius = [0,10,20,34.5]
+
+#fahrenheit = [#for temp in celcius ]
+#print(fahrenheit)
