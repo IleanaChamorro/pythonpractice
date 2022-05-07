712920d4266b44dc0a0791e8d43222ad828f9db3
ileanachamorro <ic.chamorro07@gmail.com>
2022-05-06 22:22:58 -0300

    Programacion orientada a objetos

---
+++ 7.POO/poo.py
@@ -0,0 +1,20 @@
+#Programación orientada a objetos
+#Permite a los programadores crear sus propios objetos que tienen metodos y atributos
+#Podemos llamar distintos metodos que se encuentran en una clase
+#Los metodos actuan como funciones que usan informacion sobre el objetos para poder retornar resultados
+
+#Podemos crear nuestros propios objetos
+#Podemos crear codigo repetible y organizado
+
+#Para los libretos grandes de python necesitamos algo mas que solo funciones para organizar todo
+#Tareas repetidas son definidas en clases para evitar la redundancia de codigo
+
+#Ejemplo
+#Class NombreDeClase():
+#   def __init__(self,param1,param2)
+#       self.param1 = param1
+#       self.param2 = param2
+#   
+#   def otraFuncion(self):
+#   #accion
+#   print(self.param1)
